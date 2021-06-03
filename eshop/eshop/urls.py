052f406862bb7acd9b4fc20fe2ecdafa45b6b65d
SalmanManzoor <salman.manzoor@ivylabtech.com>
2021-06-03 09:56:08 +0500

    account template added

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('product/', views.product, name='product'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('account/', views.account, name='account'),
     
 
 
